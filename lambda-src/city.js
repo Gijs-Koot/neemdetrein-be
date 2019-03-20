30e304b61ad5538fc65c8a988f6b25b8d3bedbc6
Gijs Koot <gijskoot@gmail.com>
2019-03-20 20:18:57 +0100

    first commit

---
+++ lambda-src/city.js
@@ -0,0 +1,14 @@
+const statusCode = 200;
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+exports.handler = function(event, context, callback) {
+    callback(null, {
+      statusCode,
+      headers,
+      body: 'Let there be light!'
+    });
+}
+
