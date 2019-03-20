b74f410121f5dd4f250d90e603d4f026f86c45e8
Gijs Koot <gijskoot@gmail.com>
2019-03-20 21:10:53 +0100

    city fuzzy search for autocomplete built

--- lambda-src/city.js
+++ lambda-src/city.js
@@ -4,11 +4,21 @@ const headers = {
   "Access-Control-Allow-Headers": "Content-Type"
 };
 
+
+const Fuse = require('fuse.js');
+
+var cities = require("../data/world-cities.json")
+var options = {
+    keys: ['city']
+};
+
+var fuse = new Fuse(cities, options)
+
 exports.handler = function(event, context, callback) {
     callback(null, {
-      statusCode,
-      headers,
-      body: 'Let there be light!'
+        statusCode,
+        headers,
+        body: JSON.stringify(fuse.search(event["queryStringParameters"]["q"]).slice(0, 5))
     });
 }
 
